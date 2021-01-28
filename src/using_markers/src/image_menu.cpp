0336e4638ca7e474fc52cfc8c992bbb7ca6f6a5c
aliaga <aliaga@sensetime.jp>
2021-01-28 21:20:45 +0900

    added menu

---
+++ src/using_markers/src/image_menu.cpp
@@ -0,0 +1,104 @@
+/*
+*  Trying to do rviz to show images with options determined by a menu
+*/
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include <sensor_msgs/Image.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+
+#include <interactive_markers/interactive_marker_server.h>
+#include <interactive_markers/menu_handler.h>
+
+#include <tf/transform_broadcaster.h>
+#include <tf/tf.h>
+
+#include <math.h>
+
+using namespace visualization_msgs;
+using namespace interactive_markers;
+
+boost::shared_ptr<InteractiveMarkerServer> server;
+//float marker_pos = 0;
+int found_elements = 2;  //Here we simulate we have "found" two elements
+
+MenuHandler menu_handler;
+MenuHandler::EntryHandle h_mode_last;
+
+
+void modeCb( const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
+{
+  ROS_INFO("modeCb called.");
+
+}
+
+void makeMenuMarker( std::string name )
+{
+  InteractiveMarker menu_marker;
+  menu_marker.header.frame_id = "base_link";
+  menu_marker.pose.position.y = 0.0; //-3.0 * marker_pos++;;
+  menu_marker.scale = 1;
+  menu_marker.name = name;
+
+  InteractiveMarkerControl control;
+
+  control.interaction_mode = InteractiveMarkerControl::BUTTON;
+  control.always_visible = true;
+
+  Marker marker;
+
+  marker.type = Marker::CUBE;
+  marker.scale.x = 0.45;
+  marker.scale.y = 0.45;
+  marker.scale.z = 0.45;
+  marker.color.r = 0.5;
+  marker.color.g = 0.5;
+  marker.color.b = 0.5;
+  marker.color.a = 1.0;
+
+  control.markers.push_back( marker );
+  menu_marker.controls.push_back(control);
+
+  server->insert( menu_marker );
+
+}
+
+void initMenu()
+{
+    MenuHandler::EntryHandle sub_menu_handle = menu_handler.insert( "Show" );
+    for ( int i=0; i<found_elements; i++ )
+    {
+      std::ostringstream s;
+      s << "Element " << i;
+      h_mode_last = menu_handler.insert( sub_menu_handle, s.str(), &modeCb );
+      menu_handler.setCheckState( h_mode_last, MenuHandler::CHECKED );  //The default option is checked (show everything)
+    }
+
+}
+
+int main(int argc, char** argv)
+{
+
+  ros::init(argc, argv, "image_menu");
+
+  server.reset( new InteractiveMarkerServer("image_menu","",false) );  //take ownership of the marker
+
+  initMenu();  //First we initiate the shape of the menu on the menu_handler
+
+  makeMenuMarker("Image" );
+
+  // we apply the menu hander on the server 
+  menu_handler.apply( *server, "Image" );
+
+  server->applyChanges();
+
+  ros::spin();
+
+  server.reset();  //deletes managed object
+
+
+
+
+}
